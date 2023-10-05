4afed2638dd754dabc392982b7f2b24dd64a76ea
Ella <lunar.politics@hmail.com>
2023-10-05 23:57:30 +0300

    initial

---
+++ public/js/common.js
@@ -0,0 +1,108 @@
+"use strict";
+
+const $ = jQuery;
+
+
+function eventHandler() {
+
+	JSCCommon.init()
+
+
+	function whenResize() {
+		JSCCommon.setFixedNav();
+	}
+
+	window.addEventListener('scroll', () => {
+		JSCCommon.setFixedNav();
+
+	}, { passive: true })
+	window.addEventListener('resize', () => {
+		whenResize();
+	}, { passive: true });
+
+	whenResize();
+
+
+	let defaultSl = {
+		spaceBetween: 0,
+		lazy: {
+			loadPrevNext: true,
+		},
+		watchOverflow: true,
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: ' .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+			// renderBullet: function (index, className) {
+			// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+			// }
+		},
+	}
+
+	new Swiper('.breadcrumb-slider--js', {
+		slidesPerView: 'auto',
+		freeMode: true,
+		watchOverflow: true
+	});
+
+	const productSliders = document.querySelectorAll('.product-slider');
+	productSliders.forEach((slider) => {
+		const productSlider = new Swiper(slider,{
+			slidesPerView: 'auto',
+			spaceBetween: 20,
+			breakpoints: {
+				768: {
+					spaceBetween: 40,
+				},
+			},
+			navigation: {
+				nextEl: slider.querySelector('.swiper-button-next'),
+				prevEl: slider.querySelector('.swiper-button-prev'),
+			},
+			scrollbar: {
+				el: slider.querySelector('.swiper-scrollbar'),
+				draggable: true,
+			},
+		});
+	})
+
+	const projectSlider = new Swiper('.sProjects__slider', {
+		slidesPerView: 1,
+		// centeredSlides: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	})
+
+	const swiper4 = new Swiper('.sBanners__slider--js', { // если не используешь методы swiper  - можно обращаться без нее к Swiper
+		// slidesPerView: 5,
+		...defaultSl,
+		slidesPerView: 'auto',
+		freeMode: true,
+		loopFillGroupWithBlank: true,
+		touchRatio: 0.2,
+		slideToClickedSlide: true,
+		freeModeMomentum: true,
+
+	});
+
+};
+if (document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
+}
+
+// window.onload = function () {
+// 	document.body.classList.add('loaded_hiding');
+// 	window.setTimeout(function () {
+// 		document.body.classList.add('loaded');
+// 		document.body.classList.remove('loaded_hiding');
+// 	}, 500);
+// }
---
+++ readme.md
@@ -0,0 +1,2 @@
+1. <https://megafronteam.github.io/006camerich/public/>
+2. <https://megafronteam.github.io/006camerich/public/00-modal.html>
